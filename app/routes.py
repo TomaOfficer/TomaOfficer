f867f92886fde1c3d4a02d161db45b2968cfcf7b
Toma <Thomas.officer@gmail.com>
2023-11-27 09:25:18 -0500

    major refactor

--- app/routes.py
+++ app/routes.py
@@ -10,4 +10,46 @@ def home():
 def about():
     return render_template('about.html')
 
-# Define other routes similarly
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/portfolio/afterpattern')
+def afterpattern():
+    return render_template('portfolio/afterpattern.html')
+
+@app.route('/portfolio/blueridgelabs')
+def blueridgelabs():
+    return render_template('portfolio/blueridgelabs.html')
+
+@app.route('/blog/aicontext')
+def aicotext():
+    return render_template('blog/aicontext.html')
+
+@app.route('/blog/ai-chatbot')
+def aichatbot():
+    return render_template('blog/ai-chatbot.html')
+
+@app.route('/blog/legal-apps')
+def legalapps():
+    return render_template('blog/legal-apps.html')
+
+@app.route('/blog/problems-and-ideas')
+def problemsandideas():
+    return render_template('blog/problems-and-ideas.html')
+
+@app.route('/blog/start-of-an-idea')
+def startofanidea():
+    return render_template('blog/start-of-an-idea.html')
+
+@app.route('/blog/system-message')
+def systemmessage():
+    return render_template('blog/system-message.html')
