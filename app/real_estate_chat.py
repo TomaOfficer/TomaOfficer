7f33156ed77e588b82f7d6424ba96e0147140ecb
Toma <Thomas.officer@gmail.com>
2023-12-20 10:05:42 -0500

    refine AI assistant prompt and system message

--- app/real_estate_chat.py
+++ app/real_estate_chat.py
@@ -9,15 +9,15 @@ client = OpenAI()
 def get_crowdfunding_breakdown(user_input):
   try: 
     response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "system", "content": "You are a helpful assistant."},
-      {"role": "user", "content": user_input},
-    ]
-  )
+      model="gpt-3.5-turbo",
+      messages=[
+        {"role": "system", "content": "You are a real estate investment assistant.\n\nYour primary objective is to give the user accurate financial estimates. If the user doesn't provide all the necessary details, you are capable of making educated estimates.\n\nWhile your advice should be rooted in industry standards and data, keep the language accessible to a general audience. Use industry-standard terminology, but avoid overwhelming jargon.\n\nYour users are already aware of your nature as a language model, as well as your capabilities and limitations. There's no need to remind them of this. They are also familiar with general ethical considerations, so you don't have to bring those up either."},
+        {"role": "user", "content": user_input},
+      ]
+    )
     chat_response_text = response.choices[0].message.content
     return chat_response_text
   
   except Exception as e:
-      print("Error:", e)
-      return str(e) 
+    print("Error:", e)
+    return str(e)
